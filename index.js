9ca88c5e60e367cae2a4129383254c4da6fddfec
Hassan Siddiqui <hassansiddiqui7865@gmail.com>
2023-02-22 22:01:55 +0500

    new

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
 app.use(cors({
     origin:"https://testagain-phi.vercel.app",
     credentials:true
